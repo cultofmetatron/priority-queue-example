c290c0f4dcf35d120176334472c40850407eed9e
cultofmetatron <cultofmetatron@aumlogic.com>
2012-09-21 19:58:17 -0400

    initial commit

---
+++ priorityqueue.py
@@ -0,0 +1,33 @@
+
+#an implementation of a priority queue
+
+class PriorityQueue(self):
+    def __init__(self):
+        self.heap = []
+
+    def insert(self, item):
+        pass
+
+class Heap(self):
+    """a bottom up heap"""
+    def __init__(self, comparator):
+        self.root = {}
+        self.size == 0
+        self.cmp = comparator
+
+    def insert(self, item):
+        self.size = self.size + 1
+        if self.root = {}:
+            self.root["item"] = item
+        else
+
+
+
+
+#main fnunctions for producing tests
+def main():
+    print "hello"
+
+if __name__ == "__main__":
+    main()
+
